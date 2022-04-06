24196c4a86e1818937650d97c3e034cf5e9ed9d2
lorenzored98 <lore.pepe998@gmail.com>
2022-04-06 11:37:29 +0200

    w

---
+++ src/lib/gl.ts
@@ -0,0 +1,57 @@
+export const createShader = (
+	gl: WebGL2RenderingContext,
+	type: "vert" | "frag",
+	source: string
+) => {
+	let t = -1;
+	if (type === "vert") {
+		t = gl.VERTEX_SHADER;
+	} else if (type === "frag") {
+		t = gl.FRAGMENT_SHADER;
+	}
+
+	const shader = gl.createShader(t);
+	if (!shader) {
+		throw new Error("Could not create shader");
+	}
+
+	gl.shaderSource(shader, source);
+	gl.compileShader(shader);
+
+	const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+
+	if (success) {
+		return shader;
+	}
+
+	const error = gl.getShaderInfoLog(shader);
+
+	gl.deleteShader(shader);
+	throw new Error(error || "Could not compile shader");
+};
+
+export const createProgram = (
+	gl: WebGL2RenderingContext,
+	{ vert, frag }: { vert: WebGLShader; frag: WebGLShader }
+) => {
+	const program = gl.createProgram();
+	if (!program) {
+		throw new Error("Could not create program");
+	}
+
+	gl.attachShader(program, vert);
+	gl.attachShader(program, frag);
+	gl.linkProgram(program);
+
+	const success = gl.getProgramParameter(program, gl.LINK_STATUS);
+
+	if (success) {
+		gl.deleteShader(vert);
+		gl.deleteShader(frag);
+		return program;
+	}
+
+	const error = gl.getProgramInfoLog(program);
+	gl.deleteProgram(program);
+	throw new Error(error || "Could not link program");
+};
